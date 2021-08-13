d6af05ceff01d871ede5f7e39295010a3aebf9d1
Theuri0411 <theuri007@students.uonbi.ac.ke>
2021-08-13 13:07:31 +0300

    Update the test function

--- OOP.py
+++ OOP.py
@@ -6,13 +6,3 @@ class Person:
 
     def the_person(self):
         print(self.name, self.age, self.salary)
-
-
-Floyd = Person("Floyd", 15, 36000)
-Jeff = Person("Jeff", 22, 80000)
-
-Jeff.name = "Eminem"
-Floyd.age = 1000
-Floyd.the_person()
-Jeff.the_person()
-
--- Test_Function.py
+++ Test_Function.py
@@ -1,16 +1,27 @@
 import unittest
-import tax_calculator as tx
+import tax_calculator as tc
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        ##Test Tax_Calculator
+    def test_net_pay_calculations(self):
+        """Test Net Pay"""
+        net_pay = tc.calc_tax(100, 10)
+        self.assertEqual(90, net_pay)
 
-        # net_pay = tx.cal_tax(10, 100)
-        net_pay_age = tx.cal_tax(100, 10)
-
-        # self.assertEqual(90, net_pay)
+    def test_tax_age_bracket_45(self):
+        """Test tax with the age bracket of less than 46 years"""
+        net_pay_age = tc.total_calc_tax(100, 45)
         self.assertEqual(91, net_pay_age)
+
+    def test_tax_age_bracket_65(self):
+        """Test tax with the age bracket of greater than 45 less than 66 years"""
+        net_pay_age = tc.total_calc_tax(100, 65)
+        self.assertEqual(95, net_pay_age)
+
+    def test_tax_age_bracket_above_65(self):
+        """Test tax with the age bracket of greater than 65 years"""
+        net_pay_age = tc.total_calc_tax(100, 66)
+        self.assertEqual(97, net_pay_age)
 
 
 if __name__ == '__main__':
